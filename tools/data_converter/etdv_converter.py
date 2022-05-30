eba7c10c6ef0164ec85fd66b14da214a38d5105c
AlexPasqua <a.pasquali5@studenti.unipi.it>
2022-05-30 16:24:57 +0200

    Substituted dummy 'get_num_points_in_gt' with a real one (not sure it's completely correct)

--- tools/data_converter/etdv_converter.py
+++ tools/data_converter/etdv_converter.py
@@ -3,13 +3,58 @@ import numpy as np
 from pathlib import Path
 
 from .etdv_data_utils import get_etdv_pc_info
+from mmdet3d.core.bbox import box_np_ops
 
 
-def _calculate_num_points_in_gt(infos):
+# def _calculate_num_points_in_gt(infos):
+#     for info in mmcv.track_iter_progress(infos):
+#         annos = info['annos']
+#         num_obj = len([n for n in annos['gt_names']])     # if n != 'DontCare'])
+#         annos['num_points_in_gt'] = -np.ones(num_obj).astype(np.int32)
+
+
+def _calculate_num_points_in_gt(data_path,
+                                infos,
+                                relative_path,
+                                remove_outside=True,
+                                num_features=4):
     for info in mmcv.track_iter_progress(infos):
+        pc_info = info['lidar_points']
+        # image_info = info['image']
+        # calib = info['calib']
+        if relative_path:
+            v_path = str(Path(data_path) / pc_info['lidar_path'])
+        else:
+            v_path = pc_info['lidar_path']
+        points_v = np.fromfile(
+            v_path, dtype=np.float32, count=-1).reshape([-1, num_features])
+        # rect = calib['R0_rect']
+        # Trv2c = calib['Tr_velo_to_cam']
+        # P2 = calib['P2']
+        # if remove_outside:
+        #     points_v = box_np_ops.remove_outside_points(
+        #         points_v, rect, Trv2c, P2, image_info['image_shape'])
+
+        # points_v = points_v[points_v[:, 0] > 0]
         annos = info['annos']
-        num_obj = len([n for n in annos['gt_names']])     # if n != 'DontCare'])
-        annos['num_points_in_gt'] = -np.ones(num_obj).astype(np.int32)
+        num_obj = len([n for n in annos['gt_names'] if n != 'DontCare'])
+        # annos = kitti.filter_kitti_anno(annos, ['DontCare'])
+        dims = annos['dimensions'][:num_obj]
+        loc = annos['location'][:num_obj]
+        rots = annos['rotation_y'][:num_obj]
+        gt_boxes_lidar = np.concatenate([loc, dims, rots[..., np.newaxis]], axis=1)
+        # gt_boxes_lidar = box_np_ops.box_camera_to_lidar(
+        #     gt_boxes_camera, rect, Trv2c)
+        indices = box_np_ops.points_in_rbbox(points_v[:, :3], gt_boxes_lidar)
+
+        # print('\n\n',np.where(indices),'\n\n')
+        # exit()
+
+        num_points_in_gt = indices.sum(0)
+        num_ignored = len(annos['dimensions']) - num_obj
+        num_points_in_gt = np.concatenate(
+            [num_points_in_gt, -np.ones([num_ignored])])
+        annos['num_points_in_gt'] = num_points_in_gt.astype(np.int32)
 
 
 def _read_imageset_file(path):
@@ -60,7 +105,7 @@ def create_etdv_info_file(data_path,
     # For now this func is a dummy that puts a dummy value as the number of points in each box:
         # this dummy value might be 0, 1, n, or -1 (-1 is currently used for boxes of class DontCare)
         # currently using -1
-    _calculate_num_points_in_gt(etdv_infos_train)
+    _calculate_num_points_in_gt(data_path, etdv_infos_train, relative_path)
 
     filename = save_path / f'{pkl_prefix}_infos_train.pkl'
     print(f'ETDV info train file is saved to {filename}')
@@ -73,7 +118,7 @@ def create_etdv_info_file(data_path,
         pc_ids=val_pc_ids,
         relative_path=relative_path
     )
-    _calculate_num_points_in_gt(etdv_infos_val)
+    _calculate_num_points_in_gt(data_path, etdv_infos_val, relative_path)
     filename = save_path / f'{pkl_prefix}_infos_val.pkl'
     print(f'ETDV info val file is saved to {filename}')
     mmcv.dump(etdv_infos_val, filename)
